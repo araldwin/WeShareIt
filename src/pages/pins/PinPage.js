748342ca529e6e93bd88ce3f36c626aa017f3f9d
araldwin <araldwin@gmail.com>
2023-09-06 09:08:28 +0000

    create CommentCreateForm and add it to PinPage

--- src/pages/pins/PinPage.js
+++ src/pages/pins/PinPage.js
@@ -9,18 +9,26 @@ import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Pin from "../pins/Pin";
 
+import CommentCreateForm from "../comments/ CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
 function PinPage() {
   const { id } = useParams();
   const [pin, setPin] = useState({ results: [] });
 
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
+
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: pin }] = await Promise.all([
+        const [{ data: pin }, {data: comments}] = await Promise.all([
           axiosReq.get(`/pins/${id}`),
+          axiosReq.get(`/comments/?pin=${id}`)
         ]);
         setPin({ results: [pin] });
-        console.log(pin);
+        setComments(comments)
       } catch (err) {
         console.log(err);
       }
@@ -34,7 +42,30 @@ function PinPage() {
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
         <Pin {...pin.results[0]} setPin={setPin} pinPage />
-        <Container className={appStyles.Content}>Comments</Container>
+        <Container className={appStyles.Content}>
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              pin={id}
+              setPin={setPin}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
+          {comments.results.length ? (
+            comments.results.map(comment => (
+              <p key={comment.id}>
+                {comment.owner}: {comment.content}
+              </p>
+            ))
+          ) : currentUser ? (
+           <span>No comments yet, be the first to comment!</span> 
+          ) : (
+            <span>No comments... yet</span>
+          )}
+        </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
         Popular profiles for desktop
