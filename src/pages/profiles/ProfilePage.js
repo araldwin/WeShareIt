244e6185e5fba598bf4684d69f60ecf6f718ad8d
araldwin <araldwin@gmail.com>
2023-09-19 16:48:20 +0000

    add function for following profiles

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -25,25 +25,24 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import Pin from "../pins/Pin";
 import { fetchMoreData } from "../../utils/data";
 
-
 function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
   const [profilePins, setProfilePins] = useState({ results: [] });
   const currentUser = useCurrentUser();
   const { id } = useParams();
-  const setProfileData = useSetProfileData();
+  const { setProfileData, handleFollow } = useSetProfileData();
   const { pageProfile } = useProfileData();
   const [profile] = pageProfile.results;
   const is_owner = currentUser?.username === profile?.owner;
- 
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const [{ data: pageProfile}, { data: profilePins} ] = await Promise.all([
-          axiosReq.get(`/profiles/${id}`),
-          axiosReq.get(`/pins/?owner__profile=${id}`),
-        ]);
+        const [{ data: pageProfile }, { data: profilePins }] =
+          await Promise.all([
+            axiosReq.get(`/profiles/${id}`),
+            axiosReq.get(`/pins/?owner__profile=${id}`),
+          ]);
         setProfileData((prevState) => ({
           ...prevState,
           pageProfile: { results: [pageProfile] },
@@ -70,30 +69,45 @@ function ProfilePage() {
           <h3 className={`${styles.ProfileName} m-2`}>{profile?.owner}</h3>
           <Row className="justify-content-center no-gutters">
             <Col xs={4} className="my-2">
-              <div className={`${styles.ProfileStats}`}>{profile?.pins_count}</div>
+              <div className={`${styles.ProfileStats}`}>
+                {profile?.pins_count}
+              </div>
               <div className={`${styles.ProfileStatsLabel}`}>Pins</div>
             </Col>
             <Col xs={4} className="my-2">
-              <div className={`${styles.ProfileStats}`}>{profile?.followers_count}</div>
+              <div className={`${styles.ProfileStats}`}>
+                {profile?.followers_count}
+              </div>
               <div className={`${styles.ProfileStatsLabel}`}>Followers</div>
             </Col>
             <Col xs={4} className="my-2">
-              <div className={`${styles.ProfileStats}`}>{profile?.following_count}</div>
+              <div className={`${styles.ProfileStats}`}>
+                {profile?.following_count}
+              </div>
               <div className={`${styles.ProfileStatsLabel}`}>Following</div>
             </Col>
           </Row>
         </Col>
         <Col lg={3} className="text-lg-right">
-          {currentUser && !is_owner && (
-            <Button
-              className={`${btnStyles.Button} ${btnStyles.BlackOutline}`}
-              onClick={() => {}}
-            >
-              {profile?.following_id ? "Unfollow" : "Follow"}
-            </Button>
-          )}
+          {currentUser &&
+            !is_owner &&
+            (profile?.following_id ? (
+              <Button
+                className={`${btnStyles.Button} ${btnStyles.BlackOutline}`}
+                onClick={() => {}}
+              >
+                unfollow
+              </Button>
+            ) : (
+              <Button
+                className={`${btnStyles.Button} ${btnStyles.Black}`}
+                onClick={() => handleFollow(profile)}
+              >
+                follow
+              </Button>
+            ))}
         </Col>
-        {profile?.content && <Col className={`${styles.ProfileContent} p-3`}>{profile.content}</Col>}
+        {profile?.content && <Col className="p-3">{profile.content}</Col>}
       </Row>
     </>
   );
@@ -104,20 +118,20 @@ function ProfilePage() {
       <p className="text-center">{profile?.owner}'s pins</p>
       <hr />
       {profilePins.results.length ? (
-				<InfiniteScroll
-					children={profilePins.results.map((pin) => (
-						<Pin key={pin.id} {...pin} setPins={setProfilePins} />
-					))}
-					dataLength={profilePins.results.length}
-					loader={<Spinner />}
-					hasMore={!!profilePins.next}
-					next={() => fetchMoreData(profilePins, setProfilePins)}
-				/>
-			) : (
-				<Asset
-					message={`No results found, ${profile?.owner} hasn't posted yet.`}
-				/>
-			)}
+        <InfiniteScroll
+          children={profilePins.results.map((pin) => (
+            <Pin key={pin.id} {...pin} setPins={setProfilePins} />
+          ))}
+          dataLength={profilePins.results.length}
+          loader={<Spinner />}
+          hasMore={!!profilePins.next}
+          next={() => fetchMoreData(profilePins, setProfilePins)}
+        />
+      ) : (
+        <Asset
+          message={`No results found, ${profile?.owner} hasn't posted yet.`}
+        />
+      )}
     </>
   );
 
